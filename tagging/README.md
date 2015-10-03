5f10ca446b816e91116b51807e22cdefefc1bf5b
Giuliana Veizaga <veizagagiuliana@gmail.com>
2015-10-03 17:23:06 -0300

    Ejercicio 1 primera parte

---
+++ tagging/README.md
@@ -0,0 +1,25 @@
+Practico 2
+
+Ejercicio 1: Corpus Ancora: Estadísticas de etiquetas POS
+
+sents: 17379
+occurrence of words: 517300
+vocabulary of words: 46483
+vocabulary of tags: 48
+
+
+ETIQUETAS   SIGNIFICADO           CANTIDAD      PORCENTAJE       PALABRAS MAS FRECUENTES
+    nc      Nombre común           92002    17.78503769572782%  ['años', 'presidente', 'millones', 'equipo',
+                                                                 'partido']
+    sp      Adposición preposición 79904   15.446356079644307%  ['de', 'en', 'a', 'del', 'con']
+    da      Determinante artículo  54552   10.545524840518075%  ['la', 'el', 'los', 'las', 'El']
+    vm      Verbo principal        50609    9.783297892905471%  ['está', 'tiene', 'dijo', 'puede', 'hace']
+    aq      Adjetivo calificativo  33904    6.554030543205104%  ['pasado', 'gran', 'mayor', 'nuevo', 'próximo']
+    fc      Puntuación coma        30148    5.827952832012372%  [',']
+    np      Nombre propio          29113    5.627875507442489%  ['Gobierno', 'España', 'PP', 'Barcelona',
+                                                                 'Madrid']
+    fp      Puntuación (punto,     21157    4.089889812487918%  ['.', '(', ')']
+            parentesis)
+    rg      Adverbio general       15333    2.964044075004833%  ['más', 'hoy', 'también', 'ayer', 'ya']    
+    cc      Conjunción coordinada  15023    2.904117533346221%  ['y', 'pero', 'o', 'Pero', 'e']
+
--- tagging/scripts/stats.py
+++ tagging/scripts/stats.py
@@ -8,6 +8,7 @@ Options:
   -h --help     Show this screen.
 """
 from docopt import docopt
+from collections import defaultdict
 
 from corpus.ancora import SimpleAncoraCorpusReader
 
@@ -18,6 +19,35 @@ if __name__ == '__main__':
     # load the data
     corpus = SimpleAncoraCorpusReader('ancora/ancora-2.0/')
     sents = corpus.tagged_sents()
+    words, tags = zip(*corpus.tagged_words())
 
     # compute the statistics
     print('sents: {}'.format(len(sents)))
+    print('occurrence of words: {}'.format(len(words)))
+    print('vocabulary of words: {}'.format(len(set(words))))
+    print('vocabulary of tags: {}'.format(len(set(tags))))
+
+    count_tags = defaultdict(int)
+    dicttag_withword = defaultdict(int)
+
+    for i in range(len(tags)):
+        count_tags[tags[i]] += 1
+        dicttag_withword[(tags[i], words[i])] += 1
+
+    list_ord = sorted(count_tags.items(), key=lambda tup: tup[1], reverse=True)
+    list_ordword = sorted(dicttag_withword.items(), key=lambda tup: tup[1], reverse=True)
+
+    print ('ETIQUETAS'.rjust(8), 'CANTIDAD'.rjust(10), 
+            'PORCENTAJE'.rjust(20), 'PALABRAS MAS FRECUENTES'.rjust(25))
+    for i in range(0,10):
+        tag , count = list_ord[i]
+        repeat_words = []
+        for i in range(len(list_ordword)):
+            if list_ordword[i][0][0] == tag:
+                repeat_words.append(list_ordword[i][0][1])
+            if len(repeat_words) == 5:
+                break
+        print (tag.rjust(8), repr(count).rjust(10), (str(count/len(tags)*100) + '%').rjust(22),
+               repeat_words)
+
+        
