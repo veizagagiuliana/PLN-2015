2467eb6fb54716619c28e0d58fb4743b120ad481
Giuliana Veizaga <veizagagiuliana@gmail.com>
2015-11-09 20:40:36 -0300

    ejercicio 3

--- parsing/cky_parser.py
+++ parsing/cky_parser.py
@@ -1,6 +1,5 @@
 from collections import defaultdict
 from nltk.tree import Tree
-from math import log2
 
 
 class CKYParser:
@@ -10,16 +9,19 @@ class CKYParser:
         grammar -- a binarised NLTK PCFG.
         """
 
-        self.grammar = grammar
-        self.lex = lex = []
-        self.notlex = notlex = []
         self._pi = {}
-
+        self._bp = {}
+        self.grammar = grammar
+        self.lexical = lexical = defaultdict(list)
+        self.nonlexical = nonlexical = defaultdict(list)
+       
         for elem in grammar.productions():
             if elem.is_lexical():
-                lex += [elem]
+                rhs = elem.rhs()[0]
+                lexical[rhs] += [elem]
             else:
-                notlex += [elem]
+                rhs = elem.rhs()
+                nonlexical[(rhs[0].symbol(), rhs[1].symbol())] += [elem]
 
     def parse(self, sent):
         """Parse a sequence of terminals.
@@ -29,62 +31,58 @@ class CKYParser:
         self._bp = bp = defaultdict(dict)
         n = len(sent)
         found = False
-        lex = self.lex
-        notlex = self.notlex
+        lex = self.lexical
+        nonlex = self.nonlexical
 
         for i, word in enumerate(sent, 1):
-            for elem in lex:
-                rhs = elem.rhs()
-                for k in range(len(rhs)):
-                    if word == rhs[k]:
-                        non_ter = repr(elem.lhs())
-                        log_prob = log2(elem.prob())
-                        pi[(i, i)] = {non_ter: log_prob}
-                        bp[(i, i)] = {non_ter: Tree(non_ter, [word])}
-                        found = True
-                        break
-                if found:
-                    found = False
-                    break
+            prod = lex[word]
+            for p in prod:
+                non_ter = p.lhs().symbol()
+                log_prob = p.logprob()
+                pi[(i, i)][non_ter] = log_prob
+                bp[(i, i)][non_ter] = Tree(non_ter, [word])
 
         for k in range(1, n):
             for j in range(1, n-k+1):
                 s = j + k
+                # pi[(j, s)] = {}
+                # bp[(j, s)] = {}
                 for q in range(j, s):
                     rama_i = pi[(j, q)]
                     rama_d = pi[(q+1, s)]
-                    for elem in notlex:
+                    p = []
+                    for elem1 in rama_i:
+                        for elem2 in rama_d:
+                            p += nonlex[(elem1, elem2)]
+                    for elem in p:
                         rhs = elem.rhs()
-                        izq = repr(rhs[0])
-                        der = repr(rhs[1])
-                        if izq in rama_i and der in rama_d:
-                            non_ter = repr(elem.lhs())
-                            log_prob = (log2(elem.prob()) + rama_i[izq] +
-                                        rama_d[der])
-                            child_izq = bp[(j, q)][izq]
-                            child_der = bp[(q+1, s)][der]
-                            if non_ter in pi[(j, s)]:
-                                if log_prob > pi[(j, s)][non_ter]:
-                                    pi[(j, s)] = {non_ter: log_prob}
-                                    bp[(j, s)] = {non_ter: Tree(non_ter,
-                                                  [child_izq, child_der])}
-                            else:
-                                pi[(j, s)] = {non_ter: log_prob}
-                                bp[(j, s)] = {non_ter: Tree(non_ter,
-                                              [child_izq, child_der])}
-                            found = True
-                            break
-
+                        izq =rhs[0].symbol()
+                        der =rhs[1].symbol()
+                        # if izq in rama_i and der in rama_d:
+                        non_ter = repr(elem.lhs())
+                        log_prob = elem.logprob() + rama_i[izq] + rama_d[der]
+                        child_izq = bp[(j, q)][izq]
+                        child_der = bp[(q+1, s)][der]
+                        if non_ter in pi[(j, s)]:
+                            if log_prob > pi[(j, s)][non_ter]:
+                                pi[(j, s)][non_ter] = log_prob
+                                bp[(j, s)][non_ter] = Tree(non_ter,
+                                                      [child_izq, child_der])
+                        else:
+                            pi[(j, s)][non_ter] = log_prob
+                            bp[(j, s)][non_ter] = Tree(non_ter,
+                                                       [child_izq, child_der])
+                        found = True
                 if found:
                     found = False
                 else:
+                    pi[(j, s)] = {}
                     bp[(j, s)] = {}
 
-        start = repr(self.grammar.start())
+        start = self.grammar.start().symbol()
         lp = 0.0
-        t = {}
-        if start in bp[1, n]:
+        t = None
+        if start in bp[(1, n)]:
             lp = pi[(1, n)][start]
             t = bp[(1, n)][start]
-
         return(lp, t)
